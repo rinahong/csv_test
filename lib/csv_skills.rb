5161cd048c4b4979fe761a71e91023a3203d34d7
Rina Hong <rhong1006@gmail.com>
2018-04-16 10:48:04 -0700

    V2: Refactor csv_skills.rb. Read file line by line with CSV class's foreach method.

--- lib/csv_skills.rb
+++ lib/csv_skills.rb
@@ -4,36 +4,16 @@ Bundler.require(:default)
 require 'csv'
 
 def parse_file
-  data = File.read("spec/fixtures/roster.csv")
-  rows = data.split("\n")
   domain_counts = {}
-  rows.each do |row|
-    element = row.split(",")
-    domain = element[1].partition('@').last
+  CSV.foreach("spec/fixtures/roster.csv") do |row|
+    domain = row[1].partition('@').last
     unless domain_counts.has_key?(domain)
       domain_counts[domain] = 0
     end
     domain_counts[domain] += 1
   end
-
   write_file(domain_counts)
 end
-
-# def parse_file
-#   data = File.read("spec/fixtures/roster.csv")
-#   domain_regex = /@([a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z])/
-#   domains_array = data.scan(domain_regex)
-#   domain_counts = {}
-#   domains_array.each do |domain|
-#     domain_string = domain.join()
-#     unless domain_counts.has_key?(domain_string)
-#       domain_counts[domain_string] = 0
-#     end
-#     domain_counts[domain_string] += 1
-#   end
-#   write_file(domain_counts)
-# end
-
 
 def write_file(domain_counts)
   CSV.open("output.csv", "wb") do |csv|
