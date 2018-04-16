c98ffc34b51fe8fae04db19596175bc83d0c5c48
Rina Hong <rhong1006@gmail.com>
2018-04-16 11:11:16 -0700

    V3: Create DomainCounter module. Add 2 methods to read file and extract domain names

--- lib/csv_skills_module.rb
+++ lib/csv_skills_module.rb
@@ -8,3 +8,32 @@
 #   counter.write_file("path_to_file")
 #
 # Can you update the existing code to offer this interface?
+
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+require 'csv'
+
+module DomainCounter
+  def self.read_file(read_file)
+    domain_counts = Hash.new(0)
+    CSV.foreach(read_file) do |row|
+      domain = get_domain(row[1])
+      domain_counts[domain] += 1
+    end
+    # write_file(domain_counts, "output.csv")
+  end
+
+  def self.get_domain(email_address)
+    email_address.partition('@').last
+  end
+
+  # def write_file(write_file)
+  #   CSV.open(write_file, "wb") do |csv|
+  #     csv << ["domain", "count"]
+  #     self.each do |key, value|
+  #       csv << [key, value.to_s]
+  #     end
+  #   end
+  # end
+end
