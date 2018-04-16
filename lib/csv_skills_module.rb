85674985ce2a75eae9ea9f484c8027704c1a52f8
Rina Hong <rhong1006@gmail.com>
2018-04-16 13:12:44 -0700

    Refactor csv_skills_module.rb. Define '[]' as a method

--- lib/csv_skills_module.rb
+++ lib/csv_skills_module.rb
@@ -1,18 +1,18 @@
 # NOTE Do this last.
 #
 # Imagine our interface was something like this:
-#   counter = DomainCounter.read_file("path/to/file")
-#   counter["good_domain.com"] # => 100
-#   counter["non_existent.com"] # => 0
-#   counter.to_hash # => {"domain" => 1, ...}
-#   counter.write_file("path_to_file")
+  # counter = DomainCounter.read_file("path/to/file")
+  # counter["good_domain.com"] # => 100
+  # counter["non_existent.com"] # => 0
+  # counter.to_hash # => {"domain" => 1, ...}
+  # counter.write_file("path_to_file")
 #
 # Can you update the existing code to offer this interface?
 
 # ===== How to run in pry =====
 # load './lib/csv_skills_module.rb'
-# counter = DomainCounter.read_file("spec/fixtures/roster.csv")
-# Hash.send(:include, DomainCounter)
+# counter = DomainCounter.new
+# c.read_file("spec/fixtures/roster.csv")
 # counter.write_file("output.csv")
 
 require 'rubygems'
@@ -20,26 +20,50 @@ require 'bundler/setup'
 Bundler.require(:default)
 require 'csv'
 
-module DomainCounter
-  def self.read_file(read_file)
-    domain_counts = Hash.new(0)
-    CSV.foreach(read_file) do |row|
-      domain = get_domain(row[1])
-      domain_counts[domain] += 1
-    end
-    domain_counts
+# Why did you choose a module over a class? If you did consider using
+# a class, what prevented you from being able to use it?
+# Reason why I chose module in Version 3 solution:
+# -> I wasn't sure how to call write_file for Hash. ***counter.write_file("output.csv")***
+# -> Then, I decided to add custom method (write_file) to Hash class. ***Hash.send(:include, DomainCounter)***
+# -> :include takes 'module' instead of 'class'
+
+#Version 4 Solution
+class DomainCounter
+  def initialize
+    @domain_counts = Hash.new(0)
   end
 
-  def self.get_domain(email_address)
-    email_address.partition('@').last
+  def [](key)
+    @domain_counts[key.to_s]
+  end
+
+  def []=(key, value)
+    @domain_counts[key.to_s] = value
+  end
+
+  def to_hash
+    @domain_counts
+  end
+
+  def read_file(read_file)
+    CSV.foreach(read_file) do |row|
+      domain = get_domain(row[1])
+      self[domain] += 1
+    end
   end
 
   def write_file(write_file)
     CSV.open(write_file, "wb") do |csv|
       csv << ["domain", "count"]
-      self.each do |key, value|
+      self.to_hash.each do |key, value|
         csv << [key, value.to_s]
       end
     end
   end
+
+  private
+    def get_domain(email_address)
+      email_address.partition('@').last
+    end
+
 end
