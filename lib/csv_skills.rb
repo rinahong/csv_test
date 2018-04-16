38fd388dd31a265bf11f60d152db5d120910250a
Rina Hong <rhong1006@gmail.com>
2018-04-16 10:39:53 -0700

    parse_file method is implemented.

--- lib/csv_skills.rb
+++ lib/csv_skills.rb
@@ -5,9 +5,20 @@ require 'csv'
 
 def parse_file
   data = File.read("spec/fixtures/roster.csv")
+  rows = data.split("\n")
+  domain_counts = {}
+  rows.each do |row|
+    element = row.split(",")
+    domain = element[1].partition('@').last
+    unless domain_counts.has_key?(domain)
+      domain_counts[domain] = 0
+    end
+    domain_counts[domain] += 1
+  end
 
   write_file(domain_counts)
 end
 
 def write_file(domain_counts)
+  puts domain_counts
 end
